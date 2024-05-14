64c34493f18b5f317974fde18c75b10154f0925e
pedrojonassm <pedro.jonas@athen.as>
2024-05-13 22:10:40 -0300

    adicionado botões

--- preparar sorteio comentarios instagram.js
+++ preparar sorteio comentarios instagram.js
@@ -16,8 +16,11 @@ const sleep = (milliseconds) => {
     return new Promise(resolve => setTimeout(resolve, milliseconds));
 };
 
+let lLoadingComments = false;
+
 // Função para carregar mais conteúdo
 const loadMoreContent = async () => {
+    lLoadingComments = true;
     while (!isScrollAtBottom()) {
         while (!isScrollAtBottom()) {
             // Se o scroll não estiver no final, continue rolando até o final
@@ -26,8 +29,9 @@ const loadMoreContent = async () => {
             await sleep(500); // Aguarde 0.5 segundos
         }
 
-        await sleep(3000); // Espera 3 segundos para garantir
+        await sleep(5000); // Espera 3 segundos para garantir
     }
+    lLoadingComments = false
     console.log("Fim do conteúdo alcançado. Não há mais conteúdo para carregar.");
 };
 
@@ -59,46 +63,80 @@ const generateTextContent = (commentsMap) => {
     return content;
 };
 
-// Inicie o processo de carregamento de mais conteúdo
-await loadMoreContent();
+function downloadFile() {
+    // PEGAR COMENTÁRIOS
 
+    const commentsMap = {};
 
-// PEGAR COMENTÁRIOS
+    // Seletor para encontrar os elementos que contêm o nome do usuário que comentou
+    const userElements = document.querySelectorAll("div > div > div > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(1) > span > span > div > a");
 
-const commentsMap = {};
+    // Seletor para encontrar os elementos que contêm o nome do usuário marcado no comentário
+    const mentionElements = document.querySelectorAll("div > div > div > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(2) > span > a"); // Cada a aqui é um @
 
-// Seletor para encontrar os elementos que contêm o nome do usuário que comentou
-const userElements = document.querySelectorAll("div > div > div > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(1) > span > span > div > a");
-
-// Seletor para encontrar os elementos que contêm o nome do usuário marcado no comentário
-const mentionElements = document.querySelectorAll("div > div > div > div:nth-child(1) > div:nth-child(1) > div > div:nth-child(2) > span > a"); // Cada a aqui é um @
-
-// Itera sobre os elementos para extrair os usuários e seus respectivos comentários
-userElements.forEach((userElement, index) => {
-    const user = userElement.textContent.trim();
-    const mentionElement = mentionElements[index];
-    
-    // Se o usuário já estiver no mapa, adicionamos o usuário mencionado apenas se ele ainda não estiver na lista de menções
-    if (mentionElement){
-        if (user in commentsMap) {
-            const mention = mentionElement.textContent.trim();
-            if (!commentsMap[user].includes(mention)) {
-                commentsMap[user].push(mention);
+    // Itera sobre os elementos para extrair os usuários e seus respectivos comentários
+    userElements.forEach((userElement, index) => {
+        const user = userElement.textContent.trim();
+        const mentionElement = mentionElements[index];
+        
+        // Se o usuário já estiver no mapa, adicionamos o usuário mencionado apenas se ele ainda não estiver na lista de menções
+        if (mentionElement){
+            if (user in commentsMap) {
+                const mention = mentionElement.textContent.trim();
+                if (!commentsMap[user].includes(mention)) {
+                    commentsMap[user].push(mention);
+                }
+            } else {
+                const mention = mentionElement.textContent.trim();
+                commentsMap[user] = [mention];
             }
-        } else {
-            const mention = mentionElement.textContent.trim();
-            commentsMap[user] = [mention];
         }
-    }
-});
+    });
+    // DOWNLOAD DO ARQUIVO
 
-// DOWNLOAD DO ARQUIVO
+    // Gere o conteúdo do arquivo TXT
+    const textContent = generateTextContent(commentsMap);
 
-// Gere o conteúdo do arquivo TXT
-const textContent = generateTextContent(commentsMap);
+    // Nome do arquivo TXT
+    const filename = 'sorteioUsuarios.txt';
 
-// Nome do arquivo TXT
-const filename = 'sorteioUsuarios.txt';
+    // Inicie o download do arquivo TXT
+    downloadTextFile(textContent, filename);
+}
 
-// Inicie o download do arquivo TXT
-downloadTextFile(textContent, filename);
+
+function main() {
+
+    const lDownloadButton = document.createElement("button");
+    const lLoadButton = document.createElement("button");
+
+    // Cria o botão de download
+    lLoadButton.textContent = "Carregar comentários";
+    lLoadButton.style.position = "fixed";
+    lLoadButton.style.bottom = "50px";
+    lLoadButton.style.right = "20px";
+    lLoadButton.style.zIndex = "9999";
+  
+    lLoadButton.addEventListener("click", () => {
+        loadMoreContent(); // Inicie o processo de carregamento de mais conteúdo
+    });
+
+    // Cria o botão de download
+    lDownloadButton.textContent = "Download arquivo";
+    lDownloadButton.style.position = "fixed";
+    lDownloadButton.style.bottom = "20px";
+    lDownloadButton.style.right = "20px";
+    lDownloadButton.style.zIndex = "9999";
+  
+    lDownloadButton.addEventListener("click", () => {
+        if (lLoadingComments){
+            console.log("Ainda estamos carregando o conteúdo, por favor espere.");
+        } else
+            downloadFile();
+    });
+
+    document.body.appendChild(lLoadButton);
+    document.body.appendChild(lDownloadButton);
+  }
+  
+  main();
